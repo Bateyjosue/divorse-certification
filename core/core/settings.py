503f3d5c73b9cd1d998988dafccb10db99183d49
Bateyjosue <bateyjosue19@gmail.com>
2021-09-15 07:36:36 +0200

    fix collect static

--- core/core/settings.py
+++ core/core/settings.py
@@ -122,6 +122,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT =os.path.join(BASE_DIR, 'assets')
+
+MEDIA_ROOT  = os.path.join(BASE_DIR,'media')
+MEDIA_URL   = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- core/core/urls.py
+++ core/core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from os import name
 from django.contrib import admin
 from django.urls import path, include
-# from django.conf.static import static
+from django.conf.urls.static import static
 from django.views.static import serve 
 from django.conf import settings
 from django.conf.urls import url
@@ -27,6 +27,6 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
-# +static(settings.MEDIA_URL,document_root-settings.MEDIA_ROOT)
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
 
