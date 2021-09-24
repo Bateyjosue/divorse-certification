dfd1a39e75a0facd7b1cca11033176114c7f2dd5
Bateyjosue <bateyjosue19@gmail.com>
2021-09-24 23:45:33 +0200

    admin model add

--- certification/admin.py
+++ certification/admin.py
@@ -1,24 +1,24 @@
-# from django.contrib import admin
-# from django.contrib.admin.options import ModelAdmin
+from django.contrib import admin
+from django.contrib.admin.options import ModelAdmin
 
-# from .models import Wed, Sentence, Couple,Term,Divorse
-# # Register your models here.
-# @admin.register(Wed)
-# class WedAdmin(admin.ModelAdmin):
-#     fields =[]
+from .models import Wed, Sentence, Couple,Term,Divorse
+# Register your models here.
+@admin.register(Wed)
+class WedAdmin(admin.ModelAdmin):
+    fields =[]
 
-# @admin.register(Sentence)
-# class SentenceAdmin(admin.ModelAdmin):
-#     fields =[]
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    fields =[]
 
-# @admin.register(Couple)
-# class CoupleAdmin(admin.ModelAdmin):
-#     fields =[]
+@admin.register(Couple)
+class CoupleAdmin(admin.ModelAdmin):
+    fields =[]
 
-# @admin.register(Term)
-# class TermAdmin(admin.ModelAdmin):
-#     fields =[]
+@admin.register(Term)
+class TermAdmin(admin.ModelAdmin):
+    fields =[]
 
-# @admin.register(Divorse)
-# class DivorseAdmin(admin.ModelAdmin):
-#     fields =[]
+@admin.register(Divorse)
+class DivorseAdmin(admin.ModelAdmin):
+    fields =[]
